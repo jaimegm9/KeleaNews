4f398affbe5137a978066899eb449d5da91581df
Jaime Gutierrez <>
2024-01-29 22:01:22 +0100

    Creating Workspace and Project

---
+++ KeleaNews/KeleaNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeleaNews
+//
+//  Created by jaime.gutierrez.m on 29/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeleaNews/KeleaNews/KeleaNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeleaNewsApp.swift
+//  KeleaNews
+//
+//  Created by jaime.gutierrez.m on 29/1/24.
+//
+
+import SwiftUI
+
+@main
+struct KeleaNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
